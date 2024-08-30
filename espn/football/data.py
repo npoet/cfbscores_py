9b278203169ebd56d825a4c8a64e00d55823d533
Nicholas J. Poet <nicholasjpoet@gmail.com>
2024-08-29 18:50:40 -0700

    add catch to live scoring

--- espn/football/data.py
+++ espn/football/data.py
@@ -112,21 +112,33 @@ def create_base_obj_football(input_list, game_type):
                 "home_id": i["competitions"][0]["competitors"][0]["team"]["location"],
                 "home": i["competitions"][0]["competitors"][0]["team"]["abbreviation"],
                 "home_site": i["competitions"][0]["competitors"][0]["team"]["links"][0]["href"],
-                "home_record": i["competitions"][0]["competitors"][0]["records"][0]["summary"],
                 "home_mascot": i["competitions"][0]["competitors"][0]["team"]["name"],
-                "home_logo": i["competitions"][0]["competitors"][0]["team"]["logo"],
                 "away_id": i["competitions"][0]["competitors"][1]["team"]["location"],
                 "away": i["competitions"][0]["competitors"][1]["team"]["abbreviation"],
                 "away_site": i["competitions"][0]["competitors"][1]["team"]["links"][0]["href"],
-                "away_record": i["competitions"][0]["competitors"][1]["records"][0]["summary"],
                 "away_mascot": i["competitions"][0]["competitors"][1]["team"]["name"],
-                "away_logo": i["competitions"][0]["competitors"][1]["team"]["logo"],
                 "home_score": i["competitions"][0]["competitors"][0]["score"],
                 "away_score": i["competitions"][0]["competitors"][1]["score"],
                 "time": i["status"]["type"]["shortDetail"],
                 "date": i["date"],
                 "type": game_type
             }
+            try:
+                obj["home_logo"] = i["competitions"][0]["competitors"][0]["team"]["logo"]
+            except KeyError:
+                obj["home_logo"] = None
+            try:
+                obj["away_logo"] = i["competitions"][0]["competitors"][1]["team"]["logo"]
+            except KeyError:
+                obj["away_logo"] = None
+            try:
+                obj["home_record"] = i["competitions"][0]["competitors"][0]["records"][0]["summary"]
+            except KeyError:
+                obj["home_record"] = ""
+            try:
+                obj["away_record"] = i["competitions"][0]["competitors"][1]["records"][0]["summary"]
+            except KeyError:
+                obj["away_record"] = ""
             try:
                 obj["tv"] = i["competitions"][0]["geoBroadcasts"][0]["media"]["shortName"]
             except IndexError:
