c31dc4ebfd2f9ef2084c5bda3c669481798e9eaa
Nick Poet <nick@stoneweavr.com>
2025-09-18 19:28:00 -0700

    add origins

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://npoet.dev", "https://scores.npoet.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
